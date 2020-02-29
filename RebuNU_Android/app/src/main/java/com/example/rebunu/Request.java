a96b07a46474f9f1732e39e866dfe2ab359e2536
LefanW <43128495+LefanW@users.noreply.github.com>
2020-02-29 16:41:55 -0700

    Add files via upload

---
+++ RebuNU_Android/app/src/main/java/com/example/rebunu/Request.java
@@ -0,0 +1,46 @@
+package com.example.a2;
+
+public class Request extends Record {
+    private int status;
+    private Location start;
+    private Location end;
+    private int price;
+    private int riderId;
+
+    Location getStart() {
+        return this.start;
+    }
+
+    Location getEnd() {
+        return this.end;
+    }
+
+    int getPrice() {
+        return this.price;
+    }
+
+    void setStart(Location start){
+        this.start = start;
+    }
+
+    void setEnd(Location end){
+        this.end = end;
+    }
+
+    void setPrice(int price){
+        this.price = price;
+    }
+
+    int getRiderId(){return this.riderId;}
+
+    void setRiderId(int riderId){
+        this.riderId = riderId;
+    }
+
+    int getStatus(){return this.status;}
+
+    void setStatus(int status){
+        this.status = status;
+    }
+
+}
