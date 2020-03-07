a662d2645feda538c75793ff8f1647d259c351cf
ShanyeXue <49002941+ShanyeXue@users.noreply.github.com>
2020-03-06 22:46:19 -0700

    Create Rider.java

---
+++ app/src/main/java/com/example/rebunu/Rider.java
@@ -0,0 +1,21 @@
+package com.example.rebunu;
+
+public class Rider {
+    private Request fetchRequest;
+
+    public void CreateRequest(){
+        return createRequest;
+    }
+
+    public Request FetchRequest(){
+        return fetchRequest;
+    }
+
+    public void cancelRequest(){
+        return cancelRequest;
+    }
+
+    public void confirmRequest(){
+        return confirmRequest;
+    }
+}
