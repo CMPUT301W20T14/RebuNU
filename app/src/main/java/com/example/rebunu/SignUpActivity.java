ef6d915aba4d8ae4314c6d5e84dd6f1ef907af57
ShanyeXue <49002941+ShanyeXue@users.noreply.github.com>
2020-03-10 00:03:35 -0600

    Login Activity perfection.

--- app/src/main/java/com/example/rebunu/SignUpActivity.java
+++ app/src/main/java/com/example/rebunu/SignUpActivity.java
@@ -5,6 +5,7 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.EditText;
 
 public class SignUpActivity extends AppCompatActivity {
 
@@ -15,9 +16,11 @@ public class SignUpActivity extends AppCompatActivity {
 
         Button button_return;
         Button button_finish;
+        EditText editText_username;
 
         button_return = findViewById(R.id.signup_button_return);
         button_finish = findViewById(R.id.signup_button_finish);
+        editText_username = findViewById(R.id.signup_edittext_name);
 
         button_return.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -30,7 +33,14 @@ public class SignUpActivity extends AppCompatActivity {
             @Override
             public void onClick(View v) {
                 // do something for registration
+                Boolean check = true;
+                if (editText_username.getText().toString().isEmpty()) {
+                    editText_username.setError(getResources().getString(R.string.username_empty));
+                    check = false;
+                }
+
             }
+
         });
     }
 }
