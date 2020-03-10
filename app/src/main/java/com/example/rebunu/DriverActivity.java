a2270d65267786a760ab3bbb4777942fd2a4bde3
AcidCannon <zxi3@ualberta.ca>
2020-03-10 02:44:17 -0600

    Different Screen for rider and driver

--- app/src/main/java/com/example/rebunu/DriverActivity.java
+++ app/src/main/java/com/example/rebunu/DriverActivity.java
@@ -9,6 +9,6 @@ public class DriverActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_driver2);
+        setContentView(R.layout.activity_driver);
     }
 }
