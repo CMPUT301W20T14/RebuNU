71ea760cbb7e303df7abb20a28aa2d258cf9071f
AcidCannon <zxi3@ualberta.ca>
2020-03-30 00:32:12 -0600

    Added some very useful helper functions to utility class. Order review is now functioning.

--- app/src/main/java/com/example/rebunu/Utility.java
+++ app/src/main/java/com/example/rebunu/Utility.java
@@ -1,16 +1,24 @@
 package com.example.rebunu;
 
+import android.content.Context;
+import android.location.Address;
+import android.location.Geocoder;
 import android.location.Location;
+import android.widget.Toast;
 
 import androidx.annotation.Nullable;
 
 import com.google.android.gms.maps.model.LatLng;
 import com.google.firebase.firestore.GeoPoint;
+import com.google.firebase.firestore.QueryDocumentSnapshot;
+import com.google.firestore.v1.StructuredQuery;
 
 import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.List;
+import java.util.Locale;
 import java.util.Map;
+import java.util.Objects;
 
 /**
  * utility method
@@ -113,29 +121,96 @@ public class Utility {
 
     public static String parseOrderStatus(Integer status, Boolean isVerbose) {
         String stat = "complete";
-        if(isVerbose){
+        if(!isVerbose){
             if(status != 5) {
-                stat = "Complete";
-            } else {
                 stat = "Incomplete";
+            } else {
+                stat = "Complete";
             }
         } else {
             if(status == -1) {
-                stat = "Order accepted, but no further action taken.";
+                stat = "Order accepted, but no further action has been taken";
             } else if(status == 1) {
-                stat = "Order canceled by rider before driver pick up.";
+                stat = "Order cancelled by rider before driver pick up";
             } else if(status == 2) {
-                stat = "Order canceled by rider after confirmed by driver.";
+                stat = "Order cancelled by rider after confirmed by driver";
             } else if (status == 3) {
-                stat = "Order confirmed by rider and driver.";
+                stat = "Order confirmed by rider and driver";
             } else if (status == 4) {
-                stat = "Order canceled by rider after order confirmed by rider and driver.";
+                stat = "Order cancelled by rider after order confirmed by rider and driver";
             } else if (status == 5) {
-                stat = "Order complete successfully.";
+                stat = "Order complete successfully";
             } else {
-                stat = "Order incomplete.";
+                stat = "Order incomplete";
             }
         }
         return stat;
+    }
+
+    public static Order documentSnapshotToOrder(QueryDocumentSnapshot document) {
+        Order order = new Order();
+        try {
+            String orderId = (String) document.getId();
+            Boolean rating = (Boolean) document.getData().get("rating");
+            String driverId = (String) document.getData().get("driverId");
+            Long status = (Long) document.getData().get("status");
+            ArrayList<GeoPoint> pos = (ArrayList<GeoPoint>) document.getData().get("pos");
+            Long price = (Long) document.getData().get("price");
+            String riderId = (String) document.getData().get("riderId");
+
+            if(rating == null) {
+                rating = true;
+            }
+
+            if(driverId == null) {
+                driverId = "Not applicable";
+            }
+
+            order.setId(orderId);
+            order.setDriverId(driverId);
+            order.setRating(rating);
+            order.setStatus(status.intValue());
+            order.setPrice(price.intValue());
+            order.setRiderId(riderId);
+
+            GeoPoint start = pos.get(0);
+            GeoPoint end = pos.get(1);
+            order.setStart(Utility.latLngToLocation(new LatLng(start.getLatitude(), start.getLongitude())));
+            order.setEnd(Utility.latLngToLocation(new LatLng(end.getLatitude(), end.getLongitude())));
+        } catch (Exception ignored) {}
+        return order;
+    }
+
+    public static String LocationToAddress(Location pos, Context context) {
+        Geocoder geocoder = new Geocoder(context, Locale.getDefault());
+        String result = String.format("%.5f", pos.getLatitude()) + ", " + String.format("%.5f", pos.getLongitude());
+        try {
+            List<Address> addresses = geocoder.getFromLocation(pos.getLatitude(), pos.getLongitude(), 1);
+            String address = addresses.get(0).getAddressLine(0);
+            String[] splitedAddress = Objects.requireNonNull(address.split(","));
+            result = splitedAddress[0] + ", " + splitedAddress[1];
+        } catch (Exception ignored) {}
+        return result;
+    }
+
+    public static HashMap<String, String> processOrderToStringHashMap(Order order, Context context) {
+        HashMap<String, String> stringHashMap = new HashMap<>();
+        stringHashMap.put("orderId", order.getId());
+        stringHashMap.put("start", Utility.LocationToAddress(order.getStart(), context));
+        stringHashMap.put("end", Utility.LocationToAddress(order.getEnd(), context));
+        stringHashMap.put("riderId", order.getRiderId());
+        stringHashMap.put("price", order.getPrice().toString());
+        stringHashMap.put("status", Utility.parseOrderStatus(order.getStatus(), true));
+        stringHashMap.put("driverId", order.getDriverId());
+        if(order.getDriverId().equals("Not applicable")) {
+            stringHashMap.put("rating", "Not applicable");
+        } else {
+            if(order.getRating()) {
+                stringHashMap.put("rating", "Good");
+            } else {
+                stringHashMap.put("rating", "Bad");
+            }
+        }
+        return stringHashMap;
     }
 }
